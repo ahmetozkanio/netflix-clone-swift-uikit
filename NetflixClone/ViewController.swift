eb873c7a58dac52a2be6ce785eca954a066fc197
ahmetozkanio <ahmetozkanio@yahoo.com>
2023-08-05 20:50:00 +0300

    initial project

---
+++ NetflixClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixClone
+//
+//  Created by Ahmet Özkan on 5.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
