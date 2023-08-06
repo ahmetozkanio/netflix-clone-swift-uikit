275bba6a2ee1ebb013bdbebcf3e18a41e23810b1
ahmetozkanio <ahmetozkanio@yahoo.com>
2023-08-06 14:24:09 +0300

    headerImageView created

---
+++ NetflixClone/Views/HeroHeaderUIView.swift
@@ -0,0 +1,33 @@
+//
+//  HeroHeaderUIView.swift
+//  NetflixClone
+//
+//  Created by Ahmet Özkan on 6.08.2023.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+
+    private let heroImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "heroImage")
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
