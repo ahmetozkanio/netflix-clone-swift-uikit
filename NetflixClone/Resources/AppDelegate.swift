83dc924c1de0c6dd7fef32966128ef5c390dd61b
ahmetozkanio <ahmetozkanio@yahoo.com>
2023-10-08 12:53:49 +0300

    CoreData created

--- NetflixClone/Resources/AppDelegate.swift
+++ NetflixClone/Resources/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        _ = persistentContainer
+        
         return true
     }
 
@@ -31,6 +35,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    
+    // MARK: Core Data stack
+    
+    lazy var persistentContainer: NSPersistentContainer = { // persistent container
+        let container = NSPersistentContainer(name: "NetflixCloneModel")
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    func saveContext() { // context manager
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
 
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Uygulama sona erdiğinde Core Data stack'ini kapat
+        self.saveContext()
+    }
+    
 }
 
